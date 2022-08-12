07dd7c7f271a72d804aaaefe1be89d6fec6b0755
Robert Zaranek <robert.zaranek@bilansjr.pl>
2022-08-12 18:34:05 +0200

    display a recipe page

---
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -0,0 +1,27 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by robertZ on 2022-08-12.
+ */
+@Controller
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
+        return "recipe/show";
+    }
+}
--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -28,5 +29,16 @@ public class RecipeServiceImpl implements RecipeService {
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
+    }
+
+    @Override
+    public Recipe findById(Long id){
+        Optional<Recipe> optionalRecipe = recipeRepository.findById(id);
+
+        if (!optionalRecipe.isPresent()){
+            throw new RuntimeException("Recipe Not Found!");
+        }
+
+        return optionalRecipe.get();
     }
 }
